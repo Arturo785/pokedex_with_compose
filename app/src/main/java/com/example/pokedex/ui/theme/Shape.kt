72a176004d50ff21790c02cdd0a94d09b3c8f685
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2022-01-08 21:34:38 -0600

    initial setup

---
+++ app/src/main/java/com/example/pokedex/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.example.pokedex.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+
+// default made by compose
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
