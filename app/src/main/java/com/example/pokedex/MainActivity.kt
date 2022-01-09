4720669cfd26a3c5fcee5607a2c6a6535c4dcabb
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2022-01-08 22:30:36 -0600

    addition of navigation and di

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -7,15 +7,63 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.example.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
+                // the container of our screens
+                // Gives us the reference of the nav and with the remember is not
+                // recreated every recomposition
+                // and can be used in any other screens that uses this activity
+                val navController = rememberNavController()
 
+                // in compose we can pass the name of the screen instead of the
+                // layout as in xml
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon_list_screen"
+                ) {
+                    composable("pokemon_list_screen") {
+                        //everything that should be inside our screen
+                    }
+                    composable(
+                        // params of our screen
+                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                        arguments = listOf(
+                            navArgument("dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument("pokemonName") {
+                                type = NavType.StringType
+                            },
+                        )
+                    ) {
+                        //everything that should be inside our screen
+
+                        // Retrieval of our params
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
+                }
             }
         }
     }
